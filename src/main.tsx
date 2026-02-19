3e72567225f52a1aae08d5b0b3aa2ad5208b6ed7
Saiprasad Medishetti <saiprasad.medishetti@sjp.co.uk>
2026-02-19 10:09:26 +0000

    logs

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import './index.css'
 import App from './App.tsx'
 import { Auth0Provider } from '@auth0/auth0-react'
 
+console.log({
+  domain: import.meta.env.VITE_AUTH0_DOMAIN,
+  client: import.meta.env.VITE_AUTH0_CLIENT_ID,
+
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Auth0Provider
