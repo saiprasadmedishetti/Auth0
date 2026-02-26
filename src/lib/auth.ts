bb2a48f4ab8d3853a2e309e211c743490550a6f2
Saiprasad Medishetti <saiprasad.medishetti@sjp.co.uk>
2026-02-26 09:50:41 +0000

    revoke

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,6 @@ export const auth = new WebAuth({
   domain: import.meta.env.VITE_AUTH0_DOMAIN,
   clientID: import.meta.env.VITE_AUTH0_CLIENT_ID,
   responseType: "token",
-  // redirectUri: window.location.href + "profile",
+  redirectUri: window.location.href + "profile",
   scope: "openid email profile",
 });
